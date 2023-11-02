f4dfad4ccb5fdfd7f3223c4d393221650f84d72a
Peter Vidos <peter.vidos@gmail.com>
2023-11-02 18:58:28 +0100

    Update olympic_medals.py

--- olympic_medals.py
+++ olympic_medals.py
@@ -7,6 +7,9 @@ from streamlit_vizzu import Config, Data, Style, VizzuChart
 
 st.set_page_config(layout="wide")
 
+st.title("I'll be around if you have questions or you can visit [ipyvizzu.com](https://ipyvizzu.com) for more info")
+st.title("Check out this app at [olympic-medals.streamlit.app](https://olympic-medals.streamlit.app)")
+
 data_frame = pd.read_csv("data/Data Olympics.csv", dtype={"game_year": str})
 data = Data()
 data.add_df(data_frame)
@@ -62,36 +65,36 @@ else:
 #bottom = st.empty()
 #with bottom.container():
 
-	#select countries
-	top10countries = ['USA', 'Germany', 'Soviet Union', 'Great Britain', 'France', 'China', 'Italy', 'Australia', 'Hungary', 'Japan']
+#select countries
+top10countries = ['USA', 'Germany', 'Soviet Union', 'Great Britain', 'France', 'China', 'Italy', 'Australia', 'Hungary', 'Japan']
 
-	selected_countries: List[str] = st.multiselect('Countries',options=data_frame.sort_values(by="country_name").country_name.unique(),default=top10countries)
+selected_countries: List[str] = st.multiselect('Countries',options=data_frame.sort_values(by="country_name").country_name.unique(),default=top10countries)
 
-	filter_countries = (
-		"(" + " || ".join([f"record['country_name'] == '{item}'" for item in selected_countries]) + ")"
-	)
+filter_countries = (
+	"(" + " || ".join([f"record['country_name'] == '{item}'" for item in selected_countries]) + ")"
+)
 
-	# -- concat filters --
-	filters_used = [f for f in [filter_countries, filter_year, filter_event, filter_medals] if f is not None]
-	filter = " && ".join(filters_used)
+# -- concat filters --
+filters_used = [f for f in [filter_countries, filter_year, filter_event, filter_medals] if f is not None]
+filter = " && ".join(filters_used)
 
-	if compare_by == "by Country":
-		y = ["country_name"]
-		x = ["Count"]
-		color = ["country_name"]
-		sort = "byValue"
+if compare_by == "by Country":
+	y = ["country_name"]
+	x = ["Count"]
+	color = ["country_name"]
+	sort = "byValue"
 
-	elif compare_by == "by Year":
-		y = ["Count"]
-		x = ["game_year"]
-		color = None
-		sort = "none"
+elif compare_by == "by Year":
+	y = ["Count"]
+	x = ["game_year"]
+	color = None
+	sort = "none"
 
-	else:
-		y = ["discipline_title"]
-		x = ["Count"]
-		color = ["discipline_title"]
-		sort = "byValue"
+else:
+	y = ["discipline_title"]
+	x = ["Count"]
+	color = ["discipline_title"]
+	sort = "byValue"
 
 
 config = {
